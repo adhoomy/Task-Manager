464d43e81364b732d06cb2e172726c86449d91f0
Adham Mustafa <adhoomy.mustafa@gmail.com>
2025-06-04 19:27:46 -0400

    Finished basic functionality.

---
+++ app.js
@@ -0,0 +1,28 @@
+const connectDB = require('./db/connect')
+const express = require('express')
+const tasks = require('./routes/tasks')
+require('dotenv').config()
+const notFound = require('./middleware/not-found')
+
+const app = express()
+
+// middleware
+app.use(express.static('./public'))
+app.use(express.json())
+
+// routes
+app.use('/api/v1/tasks', tasks)
+app.use(notFound)
+
+const port = 3000
+
+const start = async() => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server is on port ${port}`))
+    } catch(error) {
+        console.log(err)
+    }
+}
+
+start()
