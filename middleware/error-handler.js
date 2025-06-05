29f785b677152026de77ab1240bc6fd244901fe1
Adham Mustafa <adhoomy.mustafa@gmail.com>
2025-06-04 20:30:14 -0400

    Added custom error handling for not finding a task with an ID.

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,10 @@
+const {createCustomError, CustomError} = require('../errors/custom-errors')
+
 const errorHandler = (err, req, res, next) => {
-    return res.status(500).json({msg: err})
+    if (err instanceof CustomError) {
+        return res.status(err.statusCode).json({msg: err.message})
+    }
+    return res.status(500).json({msg: `Something went wrong, please try again.`})
 }
 
 module.exports = errorHandler
